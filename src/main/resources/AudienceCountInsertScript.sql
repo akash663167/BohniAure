82d54b1d423c7df977d441777ef5d872eb552e3f
Jacky Shevkani <shevkani.jacky@gmail.com>
2022-05-14 14:41:24 +0530

    Added Insert Scripts

---
+++ src/main/resources/AudienceCountInsertScript.sql
@@ -0,0 +1,174 @@
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Andheri', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Airoli', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Bandra', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Belapur', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Bhandup', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Borivali', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Byculla', 'Mumbai', 700);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Charni Road', 'Mumbai', 800);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Chembur', 'Mumbai', 900);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Colaba', 'Mumbai', 1000);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Dadar', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Dahisar', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Diva', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Dombivli', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Fort', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Juinagar', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Ghansoli', 'Mumbai', 700);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Ghatkopar', 'Mumbai', 800);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Vidyavihar', 'Mumbai', 900);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Goregaon', 'Mumbai', 1000);
+
+---------------------------------------------------------------------
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Jogeshwari', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Juhu', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Kalwa', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Kanjurmarg', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Khar', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Koparkhairane', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Kharghar', 'Mumbai', 700);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Malabar Hill', 'Mumbai', 800);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Mahalaxmi', 'Mumbai', 900);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Madh', 'Mumbai', 1000);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Lower Parel', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Kurla', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Malad', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Marine Lines', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Masjid', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Matunga', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Mira Bhayandar', 'Mumbai', 700);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Nariman Point', 'Mumbai', 800);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Nerul', 'Mumbai', 900);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Mumbra', 'Mumbai', 1000);
+
+---------------------------------------------------------------------
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Mumbai Central', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Mulund', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Powai', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Navi', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Saki Naka', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Santacruz', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Sewri', 'Mumbai', 700);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Sion', 'Mumbai', 800);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Thane', 'Mumbai', 900);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Turbhe', 'Mumbai', 1000);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Vashi', 'Mumbai', 100);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Vikhroli', 'Mumbai', 200);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Vile Parle', 'Mumbai', 300);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Wadala', 'Mumbai', 400);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Worli', 'Mumbai', 500);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Dharavi', 'Mumbai', 600);
+
+INSERT INTO AREAWISEAUDIENCECOUNT (areaName, cityName, audienceCount)
+VALUES ('Nahur', 'Mumbai', 700);
